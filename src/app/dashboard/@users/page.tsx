92c647c6bcf933d5cb503af76f5dc607ca4636ff
LeanQChris <chris.leanq@gmail.com>
2024-02-26 16:30:28 +0545

    feat: wip parallel routes in details

--- src/app/dashboard/@users/page.tsx
+++ src/app/dashboard/@users/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function UsersPage() {
     return (
-        <div>UsersPage</div>
+        <div>
+            <h1>UsersPage</h1>
+            <Link href={"/dashboard/detail"}>asd</Link>
+        </div>
     )
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function DashboardLayout({ children, users }:
-    { children: React.ReactNode, users: React.ReactNode }
+export default function DashboardLayout({ children, users, blogs }:
+    { children: React.ReactNode, users: React.ReactNode, blogs: React.ReactNode }
 ) {
     return (
         <div>
             <div>{children}</div>
             <div>{users}</div>
+            <div>{blogs}</div>
         </div>
     )
 }
